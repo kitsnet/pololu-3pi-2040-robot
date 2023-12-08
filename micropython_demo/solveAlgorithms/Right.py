f303cb9758d6e272b334d801ac440bf36ed3bf7f
Cornel Mercan <cornel.mercan@esrlabs.com>
2023-12-08 12:15:44 +0100

    Add move Right alg

---
+++ micropython_demo/solveAlgorithms/Right.py
@@ -0,0 +1,25 @@
+import API
+import sys
+
+def log(string):
+    sys.stderr.write("{}\n".format(string))
+    sys.stderr.flush()
+
+
+def playMusic():
+    final_countdown = "!L16 V8 c<bc4<f4 r d16c16d8c8<b4 r d16c16d4<f4 r <b16<a16<b8<a8<f8<b8 r"
+    buzzer = robot.Buzzer()
+    buzzer.play_in_background(final_countdown)
+    buzzer.off()
+
+
+playMusic()
+log("Running...")
+#API.setColor(0, 0, "G")
+#API.setText(0, 0, "abc")
+while True:
+    if not API.wallRight():
+        API.turnRight()
+    while API.wallFront():
+        API.turnLeft()
+    API.moveForward()
