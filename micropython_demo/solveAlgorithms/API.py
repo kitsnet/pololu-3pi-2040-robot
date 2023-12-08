b899688618e615bad919851c72d42dc2e247c4e2
Frincu <alexandru.frincu@accenture.com>
2023-12-08 18:20:04 +0200

    Update wall functions in API

--- micropython_demo/solveAlgorithms/API.py
+++ micropython_demo/solveAlgorithms/API.py
@@ -1,5 +1,6 @@
 from pololu_3pi_2040_robot import robot
 import time
+from tof import SensorInterface
 
 DEFAULT_SPEED_CHANGE = 250
 SPEED = 1000
@@ -32,7 +33,9 @@ class API:
         self.display.text(f"Angle:", 0, 32, 1)
 
     def handle_turn_or_stop(self, angle):
-        self.target_angle = self.robot_angle + angle
+        global target_angle, drive_motors
+        global last_time_far_from_target, last_time_gyro_reading
+        target_angle = self.robot_angle + angle
         drive_motors = not drive_motors
         if drive_motors:
             self.display.fill(1)
@@ -40,9 +43,9 @@ class API:
             self.display.text("WATCH OUT", 27, 30, 0)
             self.display.show()
             time.sleep_ms(500)
-            self.last_time_far_from_target = time.ticks_ms()
+            last_time_far_from_target = time.ticks_ms()
         self.draw_text()
-        self.last_time_gyro_reading = time.ticks_us()
+        last_time_gyro_reading = time.ticks_us()
 
 
     def mazeWidth():
@@ -52,13 +55,44 @@ class API:
         return 16 #command(args=["mazeHeight"], return_type=int)
 
     def wallFront():
-        return False #command(args=["wallFront"], return_type=bool)
+        display = robot.Display()
+        sensor = SensorInterface()
+        distance = sensor.getSensorData(2)
+        display.fill(0)
+        display.text("Wall front", 0, 0)
+        display.text(" Sensor2: " + str(distance), 0, 8)
+        display.show()
+        if distance < 70:
+            return True
+        else:
+            return False
 
     def wallRight():
-        return False #command(args=["wallRight"], return_type=bool)
+        sensor = SensorInterface()
+        display = robot.Display()
+        distance = sensor.getSensorData(1)
+        display.fill(0)
+        display.text("Wall Right", 0, 0)
+        display.text(" Sensor1: " + str(distance), 0, 8)
+        display.show()
+        if distance < 70:
+            return True
+        else:
+            return False
 
     def wallLeft():
-        return False #command(args=["wallLeft"], return_type=bool)
+        sensor = SensorInterface()
+        display = robot.Display()
+        distance = sensor.getSensorData(3)
+        display.fill(0)
+        display.text("Wall Left", 0, 0)
+        display.text(" Sensor3: " + str(distance), 0, 8)
+        display.show()
+
+        if distance < 70:
+            return True
+        else:
+            return False
 
     def moveForward(self, distance):
         self.encoders.get_counts(reset=True)
@@ -90,88 +124,45 @@ class API:
         self.motors.set_speeds(0, 0)
   
     def turnRight(self):
-        while True:
-            self.draw_text()
-            # Update the angle and the turn rate.
-            if self.imu.gyro.data_ready():
-                self.imu.gyro.read()
-                self.turn_rate = self.imu.gyro.last_reading_dps[2]  # degrees per second
-                now = time.ticks_us()
-                if self.last_time_gyro_reading:
-                    dt = time.ticks_diff(now, self.last_time_gyro_reading)
-                    self.robot_angle += self.turn_rate * dt / 1000000
-                self.last_time_gyro_reading = now
-
-                self.handle_turn_or_stop(-angle_to_turn)
-
-            # Decide whether to stop the motors.
-            if self.drive_motors:
-                far_from_target = abs(self.robot_angle - self.target_angle) > 3
-                if far_from_target:
-                    last_time_far_from_target = time.ticks_ms()
-                elif time.ticks_diff(time.ticks_ms(), last_time_far_from_target) > 250:
-                    self.drive_motors = False
-                    self.draw_text()
-
-            # Show the current angle in degrees.
-            self.display.fill_rect(48, 32, 72, 8, 0)
-            self.display.text(f"{self.robot_angle - self.target_angle:>9.3f}", 48, 32, 1)
-            self.display.show()
-
-            max_speed = 3000
-            kp = 140
-            kd = 4
-
-            # Drive motors.
-            if self.drive_motors:
-                turn_speed = (self.target_angle - self.robot_angle) * kp - self.turn_rate * kd
-                if turn_speed > max_speed: turn_speed = max_speed
-                if turn_speed < -max_speed: turn_speed = -max_speed
-                self.motors.set_speeds(-turn_speed, turn_speed)
-            else:
-                self.motors.off()        
+        pass
+        
 
     def turnLeft(self):
-        while True:
-            self.draw_text()
-            # Update the angle and the turn rate.
-            if self.imu.gyro.data_ready():
-                self.imu.gyro.read()
-                self.turn_rate = self.imu.gyro.last_reading_dps[2]  # degrees per second
-                now = time.ticks_us()
-                if self.last_time_gyro_reading:
-                    dt = time.ticks_diff(now, self.last_time_gyro_reading)
-                    self.robot_angle += self.turn_rate * dt / 1000000
-                self.last_time_gyro_reading = now
+        self.draw_text()
+        # Update the angle and the turn rate.
+        if self.imu.gyro.data_ready():
+            self.imu.gyro.read()
+            turn_rate = self.imu.gyro.last_reading_dps[2]  # degrees per second
+            now = time.ticks_us()
+            if last_time_gyro_reading:
+                dt = time.ticks_diff(now, last_time_gyro_reading)
+                robot_angle += turn_rate * dt / 1000000
+            last_time_gyro_reading = now
 
-                self.handle_turn_or_stop(angle_to_turn)
+            self.handle_turn_or_stop(angle_to_turn)
 
-            # Decide whether to stop the motors.
-            if self.drive_motors:
-                far_from_target = abs(self.robot_angle - self.target_angle) > 3
-                if far_from_target:
-                    last_time_far_from_target = time.ticks_ms()
-                elif time.ticks_diff(time.ticks_ms(), last_time_far_from_target) > 250:
-                    self.drive_motors = False
-                    self.draw_text()
+        # Decide whether to stop the motors.
+        if drive_motors:
+            far_from_target = abs(robot_angle - target_angle) > 3
+            if far_from_target:
+                last_time_far_from_target = time.ticks_ms()
+            elif time.ticks_diff(time.ticks_ms(), last_time_far_from_target) > 250:
+                drive_motors = False
+                self.draw_text()
 
-            # Show the current angle in degrees.
-            self.display.fill_rect(48, 32, 72, 8, 0)
-            self.display.text(f"{self.robot_angle - self.target_angle:>9.3f}", 48, 32, 1)
-            self.display.show()
+        # Show the current angle in degrees.
+        self.display.fill_rect(48, 32, 72, 8, 0)
+        self.display.text(f"{robot_angle - target_angle:>9.3f}", 48, 32, 1)
+        self.display.show()
 
-            max_speed = 3000
-            kp = 140
-            kd = 4
-
-            # Drive motors.
-            if self.drive_motors:
-                turn_speed = (self.target_angle - self.robot_angle) * kp - self.turn_rate * kd
-                if turn_speed > max_speed: turn_speed = max_speed
-                if turn_speed < -max_speed: turn_speed = -max_speed
-                self.motors.set_speeds(-turn_speed, turn_speed)
-            else:
-                self.motors.off()
+        # Drive motors.
+        if drive_motors:
+            turn_speed = (target_angle - robot_angle) * 140 - turn_rate * 4
+            if turn_speed > 3000: turn_speed = 3000
+            if turn_speed < -3000: turn_speed = -3000
+            self.motors.set_speeds(-turn_speed, turn_speed)
+        else:
+            self.motors.off()
 
     def setWall(x, y, direction):
         pass #command(args=["setWall", x, y, direction])
--- micropython_demo/tof.py
+++ micropython_demo/tof.py
@@ -3,37 +3,46 @@ import time
 from machine import I2C, Pin
 from vl6180x import Sensor
 
-sensor1 = Pin(27, Pin.OUT)
-sensor2 = Pin(28, Pin.OUT)
-sensor3 = Pin(29, Pin.OUT)
-
 display = robot.Display()
 
-sensor1.value(0) # turn off sensor 1
-sensor2.value(0) # turn off sensor 2
-sensor3.value(0) # turn off sensor 3
-time.sleep_ms(100)
+class SensorInterface:
+  def getSensorData(self, sensorId):
+    sensor1 = Pin(27, Pin.OUT)
+    sensor2 = Pin(28, Pin.OUT)
+    sensor3 = Pin(29, Pin.OUT)
 
-i2c = I2C(id=0, scl=Pin(5), sda=Pin(4), freq=400_000)
-sensor1.value(1) # turn on sensor 1
-time.sleep_ms(100)
-tof1 = Sensor(i2c)
-tof1.address(0x30)
-sensor2.value(1) # turn on sensor 2
-time.sleep_ms(100)
-tof2 = Sensor(i2c)
-tof2.address(0x31)
-sensor3.value(1) # turn on sensor 3
-time.sleep_ms(100)
-tof3 = Sensor(i2c)
-tof3.address(0x32)
+    sensor1.value(0) # turn off sensor 1
+    sensor2.value(0) # turn off sensor 2
+    sensor3.value(0) # turn off sensor 3
+    time.sleep_ms(100)
 
-while True:
-  distance = tof1.range()
-  display.fill(0)
-  display.text(" Sensor1: " + str(distance), 0, 0)
-  distance = tof2.range()
-  display.text(" Sensor2: " + str(distance), 0, 8)
-  distance = tof3.range()
-  display.text(" Sensor3: " + str(distance), 0, 16)
-  display.show()
+    i2c = I2C(id=0, scl=Pin(5), sda=Pin(4), freq=400_000)
+    sensor1.value(1) # turn on sensor 1
+    time.sleep_ms(100)
+    tof1 = Sensor(i2c)
+    tof1.address(0x30)
+    sensor2.value(1) # turn on sensor 2
+    time.sleep_ms(100)
+    tof2 = Sensor(i2c)
+    tof2.address(0x31)
+    sensor3.value(1) # turn on sensor 3
+    time.sleep_ms(100)
+    tof3 = Sensor(i2c)
+    tof3.address(0x32)
+
+    distance = 0
+    display.fill(0)
+    if sensorId == 1:
+      distance = tof1.range()
+      display.text(" Sensor1: " + str(distance), 0, 0)
+      display.text(" Right wall " + str(distance), 0, 8)
+    elif sensorId == 2:
+      distance = tof2.range()
+      display.text(" Sensor2: " + str(distance), 0, 0)
+      display.text(" Front wall ", 0, 8)
+    elif sensorId == 3:
+      distance= tof3.range()
+      display.text(" Sensor3: " + str(distance), 0, 0)
+      display.text(" Left wall " + str(distance), 0, 8)
+    display.show()
+    return distance
